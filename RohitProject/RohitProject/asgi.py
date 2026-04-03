3b913cefeb4f5b1a6ea12f5096a580afe65beb6d
Rohit Sahu <sahurohit1605@gmail.com>
2026-04-03 21:02:08 +0530

    first commit

--- RohitProject/RohitProject/asgi.py
+++ RohitProject/RohitProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Sahilpro project.
+ASGI config for RohitProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sahilpro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RohitProject.settings')
 
 application = get_asgi_application()
